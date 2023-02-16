be382de22cca392ad85dca13de4d7a51cfe18342
Fabrice <codinglone@gmail.com>
2023-02-16 14:12:26 +0200

    Finished overview card styling

--- src/components/OverviewCard.jsx
+++ src/components/OverviewCard.jsx
@@ -4,17 +4,30 @@ import { Link } from "react-router-dom";
 
 const OverviewCard = ({ title, value, link }) => {
   return (
-  <Container className="text-blue-700 font-semibold">
-    <h1 className="text-2xl">{title}</h1>
-    <h1 className="text-4xl hover:text-5xl">{value}</h1>
-    <Link to={link} className="underline text-2xl hover:text-blue-600">View all</Link>
-  </Container>);
+    <Container className="text-blue-700 font-semibold">
+      <h1
+        className="text-2xl hover:tracking-wider"
+        style={{ transition: "0.5s ease-in-out" }}
+      >
+        {title}
+      </h1>
+      <h1
+        className="text-4xl hover:text-5xl"
+        style={{ transition: "0.5s ease-in-out" }}
+      >
+        {value}
+      </h1>
+      <Link to={link} className="underline text-2xl hover:text-blue-600 hover:tracking-widest" style={{transition: '0.5s ease-in-out'}}>
+        View all
+      </Link>
+    </Container>
+  );
 };
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content:  space-between;
+  justify-content: space-between;
   height: 50vh;
   background-color: #fff;
   box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
