85673c5fc23c04e166c70275c9f30066b60a16de
Fabrice <codinglone@gmail.com>
2023-02-16 13:47:15 +0200

    improved styles on overview card

--- src/components/OverviewCard.jsx
+++ src/components/OverviewCard.jsx
@@ -3,14 +3,17 @@ import styled from 'styled-components'
 
 const OverviewCard = ({title}) => {
   return (
-    <Container>{title}</Container>
+    <Container className='text-blue-700 font-semibold'>{title}</Container>
   )
 }
 
 const Container = styled.div`
     background-color: #fff;
     box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-
+    padding: 10px;
+    width: 25%;
+    text-align: center;
+    
 `;
 
 export default OverviewCard
